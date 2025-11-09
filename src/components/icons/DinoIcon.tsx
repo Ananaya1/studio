6e403126caae7fac7fe00a4baa71901a196273a7
Ananaya Sood <asood5@dons.usfca.edu>
2025-11-09 01:09:49 +0000

    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" shape-ren

--- src/components/icons/DinoIcon.tsx
+++ src/components/icons/DinoIcon.tsx
@@ -4,16 +4,21 @@ export function DinoIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg 
       xmlns="http://www.w3.org/2000/svg" 
-      viewBox="0 0 24 24" 
-      fill="hsl(var(--foreground))" 
-      stroke="hsl(var(--foreground))"
-      strokeWidth="1"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      width="16" 
+      height="16" 
+      shape-rendering="crispEdges"
+      viewBox="0 0 16 16"
+      fill="hsl(var(--foreground))"
       {...props}
     >
-        <path d="M15 15V11H19V10H15V9H16V6H14V9H13V10H14V11H10V10H9V12H10V14H11V15H15Z" />
-        <path d="M7 19V18H8V17H9V16H15V17H14V18H13V19H12V20H10V19H7Z" />
+      <rect x="2" y="7" width="10" height="5" />
+      <rect x="4" y="6" width="5" height="2" />
+      <rect x="9" y="4" width="5" height="3" />
+      <rect x="13" y="5" width="1" height="1" fill="#fff"/>
+      <rect x="1" y="9" width="3" height="1" />
+      <rect x="1" y="11" width="4" height="2" />
+      <rect x="6" y="11" width="4" height="2" />
+      <rect x="0" y="8" width="2" height="2" />
     </svg>
   );
 }
